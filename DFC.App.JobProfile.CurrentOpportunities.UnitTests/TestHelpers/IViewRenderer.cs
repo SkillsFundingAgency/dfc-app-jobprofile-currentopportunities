4a481eeadebc9e1d9651d0cba36fa4fb81bf7dac
gurney <Gurney@as-tech.co.uk>
2019-09-17 10:02:13 +0100

    Backed out view tests

--- DFC.App.JobProfile.CurrentOpportunities.UnitTests/TestHelpers/IViewRenderer.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections.Generic;
-
-namespace DFC.App.JobProfile.CurrentOpportunities.UnitTests.ViewTests
-{
-    public interface IViewRenderer
-    {
-        string Render(string templateValue, object model, IDictionary<string, object> viewBag);
-    }
-}
--- DFC.App.JobProfile.CurrentOpportunities/ViewModels/DocumentViewModel.cs
+++ DFC.App.JobProfile.CurrentOpportunities/ViewModels/DocumentViewModel.cs
@@ -1,5 +1,4 @@
-﻿using Microsoft.AspNetCore.Html;
-using System;
+﻿using System;
 using System.ComponentModel.DataAnnotations;
 
 namespace DFC.App.JobProfile.CurrentOpportunities.ViewModels
