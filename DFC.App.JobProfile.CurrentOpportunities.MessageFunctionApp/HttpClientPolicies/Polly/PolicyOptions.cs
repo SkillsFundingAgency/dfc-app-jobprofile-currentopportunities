b18e8602cabf4e3d016f4348606412bf59801622
gurney <Gurney@as-tech.co.uk>
2020-07-23 11:00:00 +0100

    Removed old polly policies progect as it had been moved to a diffrent core shareable location  in the solution

--- DFC.App.JobProfile.CurrentOpportunities.MessageFunctionApp/HttpClientPolicies/Polly/PolicyOptions.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace DFC.App.JobProfile.CurrentOpportunities.MessageFunctionApp.HttpClientPolicies.Polly
-{
-    public class PolicyOptions
-    {
-        public CircuitBreakerPolicyOptions HttpCircuitBreaker { get; set; }
-
-        public RetryPolicyOptions HttpRetry { get; set; }
-    }
-}
