e99b5a17e5a01a720ed58542fef04660683f5122
James Barnes <james@testtrends.co.uk>
2020-09-02 10:42:38 +0100

    Added test framework structure

---
+++ DFC.App.JobProfile.CurrentOpportunities.Tests.IntegrationTests.API/Model/ContentType/JobProfile/JobProfileSpecialism.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace DFC.App.JobProfile.CurrentOpportunities.Tests.IntegrationTests.API.Model.ContentType
+{
+    public class JobProfileSpecialism
+    {
+        public string Id { get; set; }
+
+        public string Title { get; set; }
+
+        public Uri Url { get; set; }
+
+        public string Description { get; set; }
+    }
+}
---
+++ DFC.App.JobProfile.CurrentOpportunities.Tests.IntegrationTests.API/Model/ContentType/JobProfile/Restriction.cs
@@ -0,0 +1,11 @@
+﻿namespace DFC.App.JobProfile.CurrentOpportunities.Tests.IntegrationTests.API.Model.ContentType
+{
+    public class Restriction
+    {
+        public string Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Info { get; set; }
+    }
+}
