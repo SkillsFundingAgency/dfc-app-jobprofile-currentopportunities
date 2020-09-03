ef3f43e466cdd67e52457ebfebd8fbdeaef04e76
James Barnes <james@testtrends.co.uk>
2020-09-03 17:07:35 +0100

    Added appsettings to git

---
+++ DFC.App.JobProfile.CurrentOpportunities.API.IntegrationTests/Model/Support/AppSettings.cs
@@ -0,0 +1,11 @@
+﻿namespace DFC.App.JobProfile.CurrentOpportunities.Tests.IntegrationTests.API.Model.Support
+{
+    public class AppSettings
+    {
+        public int DeploymentWaitInMinutes { get; set; }
+
+        public ServiceBusConfig ServiceBusConfig { get; set; } = new ServiceBusConfig();
+
+        public APIConfig APIConfig { get; set; } = new APIConfig();
+    }
+}
