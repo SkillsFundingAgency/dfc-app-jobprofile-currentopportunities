1db1443ac4bd2b622e9a39c11acc8e229a851a1a
Stephen Yates <SYatesSoftware@gmail.com>
2021-12-20 09:10:32 +0000

    removed circuit breaker

--- DFC.App.JobProfile.CurrentOpportunities.Data/HttpClientPolicies/Polly/CorePolicyOptions.cs
+++ DFC.App.JobProfile.CurrentOpportunities.Data/HttpClientPolicies/Polly/CorePolicyOptions.cs
@@ -5,8 +5,6 @@ namespace DFC.App.JobProfile.CurrentOpportunities.Data.HttpClientPolicies.Polly
     [ExcludeFromCodeCoverage] //This model is only used by startup in polly setup extention methods and hence can not be used in tests.
     public class CorePolicyOptions
     {
-        public CircuitBreakerPolicyOptions HttpCircuitBreaker { get; set; }
-
         public RetryPolicyOptions HttpRetry { get; set; }
 
         public RateLimitPolicyOptions HttpRateLimitRetry { get; set; }
